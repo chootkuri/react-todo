5adf43beae0308c60eab67086083a479421e5d29
chootkuri <doyadoya@sookmyung.ac.kr>
2025-02-23 22:16:01 +0900

    style 추가변경

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 export default function TodoList({todos, removeTodo, handleCheck}) {
 
   return (
-    <div>
+    <div style={{marginTop: '10px'}}>
         {todos.map(todo => (
             <TodoListItem key={todo.id} todo={todo} removeTodo={removeTodo} handleCheck={handleCheck}/>
         ))}
